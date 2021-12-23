edcd3f71bf5b8fb105585d65a5d8502686596b20
Grant Montgomery <grantm@rusondia.net>
2021-12-23 14:18:59 -0600

    starting on sudoku cell component

---
+++ src/components/SudokuCell.svelte
@@ -0,0 +1,40 @@
+<script>
+
+    let w;
+    let h;
+    let clicked = false;
+
+    const handleClick = () => {
+        clicked = true;
+    }
+</script>
+
+<div
+    on:click={handleClick}
+    bind:clientWidth={w}
+    bind:clientHeight={h}
+    style="
+        font-size: {w * .8}px;
+    "
+    class={this.clicked ? 'clicked' : null}
+>1</div>
+
+<style>
+    div {
+        aspect-ratio: 1;
+        height: 100%;
+        max-width: 90vw;
+        max-height: 90vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        background-color: var(--background-color);
+        color: var(--text-color);
+        text-align: center;
+    }
+
+    .clicked {
+        background-color: red;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import SudokuCell from '../components/SudokuCell.svelte';
+</script>
+
+<div>
+    <SudokuCell></SudokuCell>
+</div>
+
+<style>
+    :global(:root){
+        --nord-0:#2E3440;
+        --nord-1:#3b4252;
+        --nord-2:#434c5e;
+        --nord-3:#4c566a;
+        --nord-4:#d8dee9;
+        --nord-5:#3539f0;
+        --nord-6:#eceff4;
+        --nord-7:#8fbcbb;
+        --nord-8:#88c0d0;
+        --nord-9:#81a1c1;
+        --nord-10:#5e81ac;
+        --nord-11:#bf616a;
+        --nord-12:#d08770;
+        --nord-13:#ebcb8b;
+        --nord-14:#a3be8c;
+        --nord-15:#b48ead;
+
+        --text-color: var(--nord-2);
+        --background-color: var(--nord-6);
+    }
+</style>
