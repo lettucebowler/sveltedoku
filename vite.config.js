61b5f83874883fd463fc859056da464c1c7dab64
Grant Montgomery <lettucebowler@gmail.com>
2022-07-11 23:19:19 -0500

    remove pwa stuff

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { VitePWA } from 'vite-plugin-pwa';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [
-		VitePWA({
-			registerType: 'autoUpdate',
-			injectRegister: 'auto',
-			manifest: {
-				name: 'WordLettuce',
-				short_name: 'WordLettuce',
-				description: 'WordLettuce',
-				theme_color: '#2e3440',
-				icons: [
-					{
-						src: 'pwa-192x192.png',
-						sizes: '192x192',
-						type: 'image/png'
-					},
-					{
-						src: 'pwa-512x512.png',
-						sizes: '512x512',
-						type: 'image/png'
-					}
-				]
-			}
-		}),
-		sveltekit()
-	]
+	plugins: [sveltekit()]
 };
 
 export default config;
