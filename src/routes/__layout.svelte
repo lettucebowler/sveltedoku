f884649d600cf985ce8b97b992cbd0173e811367
Grant Montgomery <lettucebowler@gmail.com>
2022-07-11 23:09:50 -0500

    ugh wrong dir

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -15,9 +15,6 @@
 	];
 </script>
 
-<svelte:head>
-	<link rel="icon" type="image/png" href="/favicon.png" />
-</svelte:head>
 <PageContentContainer>
 	<Navbar {links} />
 	<slot />
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,31 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [VitePWA({ registerType: 'autoUpdate', injectRegister: 'auto' }), sveltekit()]
+	plugins: [
+		VitePWA({
+			registerType: 'autoUpdate',
+			injectRegister: 'auto',
+			manifest: {
+				name: 'WordLettuce',
+				short_name: 'WordLettuce',
+				description: 'WordLettuce',
+				theme_color: '#2e3440',
+				icons: [
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					}
+				]
+			}
+		}),
+		sveltekit()
+	]
 };
 
 export default config;
