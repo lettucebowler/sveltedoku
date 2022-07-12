8a8a04925cc8ebdc959392dacf75928cdd6c54b0
Grant Montgomery <lettucebowler@gmail.com>
2022-07-11 22:50:19 -0500

    bleh

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [VitePWA({ registerType: 'autoUpdate' }), sveltekit()]
+	plugins: [VitePWA({ registerType: 'autoUpdate', injectRegister: 'auto' }), sveltekit()]
 };
 
 export default config;
