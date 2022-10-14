185b88b39359541311467b3c1ec8906a9ecdb986
Grant Montgomery <lettucebowler@gmail.com>
2022-10-14 16:58:54 -0500

    make sudoku cells buttons for a11y stuff

--- src/lib/components/SudokuBoard.svelte
+++ src/lib/components/SudokuBoard.svelte
@@ -37,28 +37,28 @@
 		{#each chunk(lane, 9) as row, j (`${i}-${j}`)}
 			<tr class="grid w-full grid-cols-[repeat(9,_1fr)]">
 				{#each row as cell, k (`${i}-${j}-${k}`)}
-					<td
-						on:click={() => handleClick(cell.row, cell.col)}
-						class="grid aspect-square w-full select-none place-items-center border-polar-100 text-xl font-medium font-bold transition duration-150 ease-in-out hover:bg-aurora-300"
-						class:text-frost-400={cell.legal && !cell.initial && cell.number !== 0}
-						class:bg-snow-100={!cell.selected && !cell.peerCell}
-						class:bg-frost-200={cell.peerCell && !cell.selected && !cell.peerDigit}
-						class:text-aurora-100={!cell.legal && !cell.initial && cell.number !== 0}
-						class:bg-aurora-500={cell.peerDigit &&
-							cell.peerCell &&
-							!cell.selected &&
-							!cell.legal &&
-							!cell.selected}
-						class:bg-aurora-300={cell.selected}
-						class:text-polar-100={cell.initial}
-						class:bg-aurora-400={cell.success}
-						class:text-transparent={cell.number === 0}
-						class:border-r-[3px]={k % 3 === 2 && k < 8}
-						class:border-r-[1px]={k % 3 !== 2}
-						class:border-b-[3px]={j % 3 === 2 && i < 2}
-						class:border-b-[1px]={j % 3 !== 2}
-					>
-						{cell.number || ' '}
+					<td class="p-0">
+						<button
+							on:click={() => handleClick(cell.row, cell.col)}
+							class="grid aspect-square w-full select-none place-items-center border-polar-100 text-xl font-medium font-bold hover:bg-aurora-300"
+							class:text-frost-400={cell.legal && !cell.initial && cell.number !== 0}
+							class:bg-snow-100={!cell.selected && !cell.peerCell}
+							class:bg-frost-200={cell.peerCell && !cell.selected && !cell.peerDigit}
+							class:text-aurora-100={!cell.legal && !cell.initial && cell.number !== 0}
+							class:bg-aurora-500={cell.peerDigit &&
+								cell.peerCell &&
+								!cell.selected &&
+								!cell.legal &&
+								!cell.selected}
+							class:bg-aurora-300={cell.selected}
+							class:text-polar-100={cell.initial}
+							class:bg-aurora-400={cell.success}
+							class:text-transparent={cell.number === 0}
+							class:border-r-[3px]={k % 3 === 2 && k < 8}
+							class:border-r-[1px]={k % 3 !== 2}
+							class:border-b-[3px]={j % 3 === 2 && i < 2}
+							class:border-b-[1px]={j % 3 !== 2}>{cell.number || ' '}</button
+						>
 					</td>
 				{/each}
 			</tr>
