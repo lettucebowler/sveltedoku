cbc1dab99ea0774accd8c257dc5afda03db0609c
Grant Montgomery <lettucebowler@gmail.com>
2024-01-23 19:54:29 -0600

    update deps

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +11,6 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			out: 'build',
 			runtime: 'edge'
 		}),
 		inlineStyleThreshold: 4096
